548d188a0c42dc1f1a38b84e73d8fa2f5338ae73
Cristian Camargo <chris.camargo2015@gmail.com>
2022-12-22 11:00:25 -0400

    adding animation to menu mobile

--- src/components/molecules/NavigationMenuMobile.vue
+++ src/components/molecules/NavigationMenuMobile.vue
@@ -3,16 +3,18 @@
       <button class="w-8 h-[80px]" @click="toogleMenu">
         <HamburguerIcon />
       </button>
-      <nav :class="showMenu" class="transition ease-in-out delay-150">
-        <NavLink text="Home" :to="'#banner'" @click="toogleMenu"/>
-        <NavLink text="Sobre" :to="'#about'" @click="toogleMenu"/>
-        <NavLink text="Produtos" :to="'#products'" @click="toogleMenu"/>
-        <NavLink text="Depoimentos" :to="'#testimonials'" @click="toogleMenu"/>
-        <NavLink text="Contato" :to="'#contact'" @click="toogleMenu"/>
-        <button class="absolute top-6 right-6 w-4" @click="toogleMenu">
-           <CloseMenuIcon />
-        </button>
-      </nav>
+      <Transition>
+        <nav v-show="menuIsOpen" :class="menuMobileStyleAndAnimations">
+          <NavLink text="Home" :to="'#banner'" @click="toogleMenu"/>
+          <NavLink text="Sobre" :to="'#about'" @click="toogleMenu"/>
+          <NavLink text="Produtos" :to="'#products'" @click="toogleMenu"/>
+          <NavLink text="Depoimentos" :to="'#testimonials'" @click="toogleMenu"/>
+          <NavLink text="Contato" :to="'#contact'" @click="toogleMenu"/>
+          <button class="absolute top-6 right-6 w-4" @click="toogleMenu">
+            <CloseMenuIcon />
+          </button>
+        </nav>
+      </Transition>
   </div>
 </template>
 
@@ -21,6 +23,8 @@ import { defineComponent } from 'vue'
 import NavLink from '../atoms/NavLink.vue'
 import HamburguerIcon from '../atoms/HamburguerIcon.vue'
 import CloseMenuIcon from '../atoms/CloseMenuIcon.vue'
+
+import 'animate.css';
 
 export default defineComponent({
   components : { NavLink, HamburguerIcon, CloseMenuIcon },
@@ -34,13 +38,13 @@ export default defineComponent({
       this.menuIsOpen = !this.menuIsOpen;
     }
   },
-
   computed : {
-    showMenu() {
+    menuMobileStyleAndAnimations(){
       if(this.menuIsOpen){
-        return "fixed inset-0 w-full h-screen bg-white z-50 flex flex-col gap-10 justify-center items-center transition ease-in-out delay-150"
-      } else {
-        return "fixed -top-[4000px] w-full h-screen bg-white z-50 flex flex-col gap-10 justify-center items-center transition ease-in-out delay-150"
+        return "fixed inset-0 w-full h-screen bg-white z-50 flex flex-col gap-10 justify-center items-center animate__animated animate__slideInDown"
+      }
+      else {
+        return "fixed inset-0 w-full h-screen bg-white z-50 flex flex-col gap-10 justify-center items-center animate__animated animate__slideOutUp"
       }
     }
   }
