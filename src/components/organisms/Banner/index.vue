eba113ec4962e9f28cf6a992b9776cbfbcf8a8ea
Cristian Camargo <chris.camargo2015@gmail.com>
2022-12-23 11:59:30 -0400

    fix : fix banner image and banner action button

--- src/components/organisms/Banner/index.vue
+++ src/components/organisms/Banner/index.vue
@@ -6,13 +6,13 @@
             Essências da linha Via Aroma.
           </h1>
           <p class="text-ligth-text xs:text-center md:text-start">Possuímos uma vasta opções de essências e produtos para seu bem estar pessoal.</p>
-          <a href="https://whats.link/drcomesticos" class="px-10 py-4 rounded bg-rose text-white hover:brightness-90 xs:w-full md:w-auto xs:mb-4 md:mb-0">
+          <a href="https://whats.link/drcomesticos" class="px-10 py-4 rounded bg-rose text-white hover:brightness-90 xs:w-full md:w-auto xs:mb-4 md:mb-0 flex justify-center">
             Quero Conhecer
           </a>
       </div>
 
       <div class="flex xs:justify-center md:justify-end w-full"> 
-        <div class="md:block xs:hidden w-[25%] h-[30%] bg-rose absolute z-0 -mt-10"/>
+        <div class="md:block xs:hidden w-[25%] h-[30%] max-w-[400px] bg-rose absolute z-0 -mt-10"/>
         <img src="../../../assets/images/banner.png" class="z-10">
       </div>
     </div>
