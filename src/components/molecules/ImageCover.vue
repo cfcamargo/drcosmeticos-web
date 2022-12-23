5e6fd24b4c399770397086fd96506c37d7dab4d6
Cristian Camargo <chris.camargo2015@gmail.com>
2022-12-23 11:55:46 -0400

    fix : fix bug products images

--- src/components/molecules/ImageCover.vue
+++ src/components/molecules/ImageCover.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col" :class="checkImagePosition">
+  <div class="flex flex-col xs:mx-auto md:m-0" :class="checkImagePosition">
       <div class="w-[70%] h-10 bg-rose"/>
-      <img :src="`../../src/assets/images/${source}`">
+      <img :src="source">
   </div>
 </template>
 
@@ -11,8 +11,7 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   props: {
     source: {
-      type: String,
-      required: true
+      type: String
     },
     invert : {
       type: Boolean,
--- src/components/organisms/Products/index.vue
+++ src/components/organisms/Products/index.vue
@@ -7,7 +7,7 @@
         </header>
 
         <div class="grid xs:grid-cols-1 md:grid-cols-2 mt-36 xs:justify-center md:items-center gap-8">
-          <ImageCover source="products/aromatizador.png" :invert="false" />
+          <ImageCover :source="aromatizador" :invert="false" />
           <div class="flex flex-col gap-8 xs:text-center md:xs:text-center md:text-end">
             <h2 class="font-bold text-2xl text-ouro">Aromatizador Elétrico</h2>
             <p class="text-ligth-text">O Aromatizador Elétrico viabiliza o uso de de essências e óleos essenciais com praticidade, economia e segurança.</p>
@@ -21,11 +21,11 @@
             <p class="text-ligth-text">Nossas essências possuem grande qualidade e variedade, são mais de cinquenta aromas divididos em sete famílias olfativas. A embalagem do produto é de vidro cor âmbar para proteger o conteúdo, o rótulo informa o aroma, e cada aroma possui uma cor e uma estampa específica.</p>
             <p class="text-ligth-text">Combina nossos aromas para ambientes sociais, como escritórios ou salas de reuniões, assim como, por seu toque balsâmico também deixa o lar acolhedor e aconchegante.</p>
           </div>
-          <ImageCover source="products/essencia.png" :invert="true" />
+          <ImageCover :source="essencia" :invert="true" />
         </div>
 
         <div class="grid xs:grid-cols-1 md:grid-cols-2 mt-36 xs:justify-center md:items-center gap-8">
-          <ImageCover source="products/agua-perfumada.png" :invert="false" />
+          <ImageCover :source="aguaPerfumada" :invert="false" />
           <div class="flex flex-col gap-8 xs:text-center md:text-end">
             <h2 class="font-bold text-2xl text-ouro">Água Perfumada</h2>
             <p class="text-ligth-text">Versátil e prática, pode ser utilizado em cortinas, lençóis, estofados e roupas em geral, também facilita na hora de passar as roupas. Utilize também em roupas recém-lavadas, tanto no varal, quanto na secadora. Aplique em cortinas com as janelas abertas, assim o vento irá espalhar o aroma pela casa.</p>
@@ -37,11 +37,11 @@
             <h2 class="font-bold text-2xl text-ouro">Home Spray</h2>
             <p class="text-ligth-text">O Home Spray é versátil, de fácil aplicação, a intensidade é controlada, o usuário define a quantidade de essência aspergida. Pode ser utilizado em ambientes e momentos variados. Cada aspergida aromatiza 3m² e a fixação é de 30 a 60 minutos, aproximadamente.</p>
           </div>
-          <ImageCover source="products/home-spray.png" :invert="true" />
+          <ImageCover :source="homeSpray" :invert="true" />
         </div>
 
         <div class="grid xs:grid-cols-1 md:grid-cols-2 mt-36 xs:justify-center md:items-center gap-8">
-          <ImageCover source="products/difusor.png" :invert="false" />
+          <ImageCover :source="difusor" :invert="false" />
           <div class="flex flex-col gap-8 xs:text-center md:text-end">
             <h2 class="font-bold text-2xl text-ouro">Difusor de varetas</h2>
             <p class="text-ligth-text">O Difusor de Varetas aromatiza de forma contínua e suave, as varetas de madeira são ótimos veículos para a propagação do aroma. Além disso, decora o ambiente com sofisticação e elegância. Indicados para aromatização e decoração de banheiros e lavabos.</p>
@@ -54,11 +54,11 @@
             <h2 class="font-bold text-2xl text-ouro">Linha POETA By  Bioage</h2>
             <p class="text-ligth-text">Uma linha de tratamento home care vegana , livre de corantes, parabenos e metais pesados.Conta com a exclusiva TECNOLOGIA HYLADERM 10 - 10tipos de ácido hialurônico que agem na hidratação e regeneração do tecido cutâneo.</p>
           </div>
-          <ImageCover source="products/poeta.png" :invert="true" />
+          <ImageCover :source="poeta" :invert="true" />
         </div>
 
         <div class="grid xs:grid-cols-1 md:grid-cols-2 mt-36 xs:justify-center md:items-center gap-8">
-          <ImageCover source="products/suplemento.png" :invert="false" />
+          <ImageCover :source="suplemento" :invert="false" />
           <div class="flex flex-col gap-8 xs:text-center md:text-end xs:justify-center md:items-end">
             <h2 class="font-bold text-2xl text-ouro">Suplemento Lipo Redux</h2>
             <ul class="xs:w-full md:w-3/5">
@@ -77,11 +77,11 @@
             <h2 class="font-bold text-2xl text-ouro">Linha Vitamina C</h2>
             <p class="text-ligth-text">Auxilia na uniformização e rejuvenescimento da pele, combate radicais livres e protege contra agressões externas e promove uma melhora geral na textura.</p>
           </div>
-          <ImageCover source="products/vitamina.png" :invert="true" />
+          <ImageCover :source="vitamina" :invert="true" />
         </div>
 
         <div class="grid xs:grid-cols-1 md:grid-cols-2 mt-36 xs:justify-center md:items-center gap-8">
-          <ImageCover source="products/acne.png" :invert="false" />
+          <ImageCover :source="acne" :invert="false" />
           <div class="flex flex-col gap-8 xs:text-center md:text-end">
             <h2 class="font-bold text-2xl text-ouro">Linha Bioage Bio-Acne Solution</h2>
             <p class="text-ligth-text">Age no combate à acne e previne lesões acneicas por meio de tecnologia probiótica, desenvolvida para proporcionar a redução das bactérias em desequilíbrio que favorecem o surgimento da acne, regula a oleosidade excessiva e reduz a vermelhidão da acne.</p>
@@ -94,7 +94,7 @@
             <p class="text-ligth-text">Da mesma maneira que precisamos cuidar da nossa saúde, investir em uma alimentação balanceada, com nutrientes e vitaminas, praticar exercícios físicos, escovar os dentes e tomar banho, precisamos cuidar na nossa higiene íntima.</p>
             <p class="text-ligth-text">Dentro desse contexto, pensando em todos os seus cuidados e em toda a sua saúde, temos uma linha inteira para os seus cuidados íntimos, garantindo a sua higiene e disposição para enfrentar o dia ou dormir relaxada.</p>
           </div>
-          <ImageCover source="products/racco.png" :invert="true" />
+          <ImageCover :source="racco" :invert="true" />
         </div>
     </div>
   </div>
@@ -104,7 +104,33 @@
 import { defineComponent } from 'vue'
 import ImageCover from '../../molecules/ImageCover.vue'
 
+import aromatizadorImage from '../../../assets/images/aromatizador.png'
+import essenciaImage from '../../../assets/images/essencia.png'
+import aguaPerfumadaImage from '../../../assets/images/agua-perfumada.png'
+import homeSprayImage from '../../../assets/images/home-spray.png'
+import difusorImage from '../../../assets/images/difusor.png'
+import poetaImage from '../../../assets/images/poeta.png'
+import suplementoImage from '../../../assets/images/suplemento.png'
+import vitaminaImage from '../../../assets/images/vitamina.png'
+import acneImage from '../../../assets/images/acne.png'
+import raccoImage from '../../../assets/images/racco.png'
+
 export default defineComponent({
-  components : { ImageCover }
+  components : { ImageCover },
+
+  data() {
+    return {
+      aromatizador: aromatizadorImage,
+      essencia : essenciaImage,
+      aguaPerfumada : aguaPerfumadaImage,
+      homeSpray : homeSprayImage,
+      difusor : difusorImage,
+      poeta : poetaImage,
+      suplemento : suplementoImage,
+      vitamina : vitaminaImage,
+      acne : acneImage,
+      racco : raccoImage,
+    }
+  },
 })
 </script>
